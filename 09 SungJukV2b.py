cbbb0b32858b09da4c21d873df0bd7ac8b7c806a
SEONGEUN, KIM <manuma159159@gmail.com>
2023-12-21 16:14:22 +0900

    성적처리 V2b

---
+++ 09 SungJukV2b.py
@@ -0,0 +1,36 @@
+# 성적처리 프로그램 V2
+# 이름, 국어, 영어, 수학을 이용해서
+# 총점, 평균을 계산하고 출력함
+# 단, 리스트를 이용해서 학생 3명에 대해 성적처리를 진행함
+# 반복문을 이용하면 코드를 간단히 작성 가능
+# 데이터 입력 시 input 함수 사용
+
+#입력데이터 선언
+names = []
+kors = []
+engs = []
+maths = []
+
+tots = []
+avgs = []
+
+
+# 성적 데이터 입력
+for i in range(3):
+    print(f'{i+1}번째 학생데이터 입력')
+    names.append(input('이름은?:'))
+    kors.append(int(input('국어는?')))
+    engs.append(int(input('영어는?')))
+    maths.append(int(input('수학은?')))
+
+#성적처리
+for i in range (len(names)):
+    tots.append(kors[i] + engs[i] + maths[i])
+    avgs.append(tots[i] / 3)
+
+
+#결과 출력
+for i in range(len(names)):
+    print(f'이름: {names[i]:s}, 국어: {kors[i]}, 영어: {engs[i]}, 수학:{maths[i]}')
+    print(f'총점: {tots[i]:d}, 평균: {avgs[i]:.1f}')
+
