dc3b4d51dfb5649f8be8e4243aeffcce7c740734
SEONGEUN, KIM <manuma159159@gmail.com>
2024-01-03 18:07:24 +0900

    사원등록 프로그램 V2 - json

---
+++ 34 employeesV2.py
@@ -0,0 +1,21 @@
+# 사원 정보를 입력받아 employees.json 파일에 저장
+# employees.csv 파일에 저장된 사원정보를 화면 출력
+# 저장시 : 사번, 이름, 성, 이메일, 입사일, ,직책 급여, 부서번호
+# 출력시 : 사번, 이름, 직책, 부서번호
+import sys
+import pppp1144.empv2 as emp
+
+emp.load_employees()
+
+while True:
+    menu = emp.showmenu()
+
+    if menu == '1' : emp.add_employee()
+    elif menu == '2' : emp.read_employee()
+    elif menu == '3' : emp.readone_employee()
+    elif menu == '4' : emp.modify_employee()
+    elif menu == '5' : emp.remove_employee()
+    elif menu == '0' : emp.exit_program()
+    else: print('메뉴를 잘못선택하셨습니다.')
+
+
