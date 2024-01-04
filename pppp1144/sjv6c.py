e75ec92f7d718da7a3c1602c0dc7ee190b051787
SEONGEUN, KIM <manuma159159@gmail.com>
2024-01-04 09:57:10 +0900

    성적처리 프로그램 V6c json 수정 2

--- pppp1144/sjv6c.py
+++ pppp1144/sjv6c.py
@@ -2,18 +2,28 @@ import json
 import sys
 from collections import OrderedDict
 
-sjs={'response' : {'body': {'totalCount':0, 'items': []}}}
+sjs = {'response' : {'body': {'totalCount':0, 'items': []}}}
+items = []
+totalCount = 0
 # sungjuks ={'response' : {'body': {'totalCount':999, 'items': []}}}
 #sjs = {'sungjuks':[]}   # 이거로 대신하면 더 편함. show 랑 save쪽 'sungjuks'로 바꾸면 됨.
 
 # 프로그램 시작시 sungjuks.json 파일을  읽어 sjs 변수에 초기화
 def load_sungjuk():
     global sjs
+    global items
+    global totalCount
     try:        # 만약 작업중에 오류가 발생하면
         with open('sungjuks.json',encoding='utf-8')as f :
             sjs = json.load(f)
+            items = sjs['response']['body']['items']
+            totalCount = sjs['response']['body']['totalCount']
     except:
-        pass        # 프로그램 실행 중단없이 다음 코드 실행
+        items = sjs['response']['body']['items']
+        totalCount = sjs['response']['body']['totalCount']
+        # 프로그램 실행 중단없이 다음 코드 실행
+        # pass
+
 def show_menu():
     main_menu = '''
     성적처리 프로그램 v6c
@@ -32,12 +42,22 @@ def show_menu():
 
 
 
+# def read_sungjuk() :
+#     sj = OrderedDict()
+#     sj['name'] = (input('이름은?:'))
+#     sj['kor'] = int(input('국어는?'))
+#     sj['eng'] = int(input('영어는?'))
+#     sj['math'] = int(input('수학은?'))
+#     return sj
+
 def read_sungjuk() :
+    sungjuk = input('이름과 성적을 입력하세요 (예: 홍길동 99 88 99) : ')
+    data = sungjuk.split() # 빈칸으로 문자열 분리
     sj = OrderedDict()
-    sj['name'] = (input('이름은?:'))
-    sj['kor'] = int(input('국어는?'))
-    sj['eng'] = int(input('영어는?'))
-    sj['math'] = int(input('수학은?'))
+    sj['name'] = data[0]
+    sj['kor'] = int(data[1])
+    sj['eng'] = int(data[2])
+    sj['math'] = int(data[3])
     return sj
 
 def compute_sungjuk(sj):
@@ -48,13 +68,24 @@ def compute_sungjuk(sj):
     '미' if sj['avg'] >= 70 else '양' \
     '양' if sj['avg'] >= 60 else '가'
 
+# def save_sungjuk(sj):
+#     # 메모리 내에 생성된 json 객체에 방금 생성한 성적데이터 저장
+#     sjs['response']['body']['items'].append(sj)
+#     sjs['response']['body']['totalCount'] += 1 # 얘는 sjs = sungjuks 면 필요없음.
+#     # 메모리 내에 생성된 json 객체를 파일에 저장 ( w를 써야 덮어쓰기 가능)
+#     with open ('sungjuks.json', 'w', encoding='utf-8')as f:
+#         json.dump(sjs, f, ensure_ascii=False)
+
 def save_sungjuk(sj):
+    global totalCount
     # 메모리 내에 생성된 json 객체에 방금 생성한 성적데이터 저장
-    sjs['response']['body']['items'].append(sj)
+    items.append(sj)
     sjs['response']['body']['totalCount'] += 1 # 얘는 sjs = sungjuks 면 필요없음.
+    # sjs['response']['body']['totalCount'] 대신 totalCount를 쓰면 기록이 안됨. 그래서 그냥 씀.
     # 메모리 내에 생성된 json 객체를 파일에 저장 ( w를 써야 덮어쓰기 가능)
     with open ('sungjuks.json', 'w', encoding='utf-8')as f:
         json.dump(sjs, f, ensure_ascii=False)
+
 
 
 
@@ -67,21 +98,33 @@ def addSungJuk():
 
 def show_sungjuk():
     print('성적데이터 조회')
-    for sj in sjs['response']['body']['items']:
+    for sj in items: #sjs['response']['body']['items']:
         print(f"이름: {sj['name']:s}, 국어: {sj['kor']},"
               f" 영어: {sj['eng']}, 수학:{sj['math']}")
 
+
+# def showone_sungonejuk():
+#     name = input('상세 조회할 학생이름은?')
+#
+#     info = '찾는 데이터가 없어요!'
+#     for sj in sjs['response']['body']['items']:
+#         if sj['name'] == name:
+#             info = (f"{sj['name']} {sj['kor']} {sj['eng']} {sj['math']} {sj['tot']}"
+#                     f"{sj['avg']} {sj['grd']}")
+#             break # 찾고나면 검색 작업 중단
+#     print(info)
 
 def showone_sungonejuk():
     name = input('상세 조회할 학생이름은?')
 
     info = '찾는 데이터가 없어요!'
-    for sj in sjs['response']['body']['items']:
+    for sj in items:
         if sj['name'] == name:
-            info = (f"{sj['name']} {sj['kor']} {sj['eng']} {sj['math']} {sj['tot']}"
+            info = (f"{sj['name']} {sj['kor']} {sj['eng']} {sj['math']} {sj['tot']} "
                     f"{sj['avg']} {sj['grd']}")
-            break # 찾고나면 검색 작업 중단
+            break  # 찾고나면 검색 작업 중단
     print(info)
+
 def modify_sungjuk():
     return None
 
